e0b43b3b82093720d8c452899f5af5862e90eb3d
Roman Koshchei <romankoshchei@gmail.com>
2022-06-30 17:08:58 +0300

    update interface

--- Client/Pages/UserPage.xaml.cs
+++ Client/Pages/UserPage.xaml.cs
@@ -1,34 +1,124 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace Client.Pages
+namespace Client
 {
-  /// <summary>
-  /// Interaction logic for UserPage.xaml
-  /// </summary>
-  public partial class UserPage : Page
-  {
-    public UserPage()
+    /// <summary>
+    /// Логика взаимодействия для UserPage.xaml
+    /// </summary>
+    public partial class UserPage : Page
     {
-      InitializeComponent();
-      var lbi = new TextBlock();
-      lbi.Background = Brushes.DarkSlateBlue;
-      lbi.Foreground = Brushes.AliceBlue;
-      lbi.Text = "Liber";
-      lbi.Height = 30;
-      ContactsList.Items.Add(lbi);
+        public UserPage()
+        {
+            InitializeComponent();
+            var lbi = new TextBlock();
+         
+            lbi.Text = "Liber";
+           
+            ContactsList.Items.Add(lbi);
 
-      lbi = new TextBlock();
-      lbi.Text = " Hi Liber,afhagsfkhgaskgfkahsgfhagsfhgaagsjhfgajhsgfjhagsjfhgajhsfgjahgsfjhagsjfhgasjfhgajhsgfjhahfbznbcmnzbxmcbzmnxbcznbxcmabhsfahsjfhgashfgkasfkjaksjfkajsfkjaskfjgaksfghasgfhgahsfgjhgasfjhagsjf";
+            lbi = new TextBlock();
 
-      MessageList.Items.Add(lbi);
+            lbi.Text = "Aria";
+
+            ContactsList.Items.Add(lbi);
+            lbi = new TextBlock();
+
+            lbi.Text = "Koscher";
+
+            ContactsList.Items.Add(lbi);
+            lbi = new TextBlock();
+
+            lbi.Text = "Crockator";
+
+            ContactsList.Items.Add(lbi);
+             lbi = new TextBlock();
+
+            lbi.Text = "Liber";
+
+            ContactsList.Items.Add(lbi);
+
+            lbi = new TextBlock();
+
+            lbi.Text = "Aria";
+
+            ContactsList.Items.Add(lbi);
+            lbi = new TextBlock();
+
+            lbi.Text = "Koscher";
+
+            ContactsList.Items.Add(lbi);
+            lbi = new TextBlock();
+
+            lbi.Text = "Crockator";
+
+            ContactsList.Items.Add(lbi); 
+            lbi = new TextBlock();
+
+            lbi.Text = "Liber";
+
+            ContactsList.Items.Add(lbi);
+
+            lbi = new TextBlock();
+
+            lbi.Text = "Aria";
+
+            ContactsList.Items.Add(lbi);
+            lbi = new TextBlock();
+
+            lbi.Text = "Koscher";
+
+            ContactsList.Items.Add(lbi);
+            lbi = new TextBlock();
+
+            lbi.Text = "Crockator";
+
+            ContactsList.Items.Add(lbi);
+            lbi = new TextBlock();
+
+            lbi.Text = "Liber";
+
+            ContactsList.Items.Add(lbi);
+
+            lbi = new TextBlock();
+
+            lbi.Text = "Aria";
+
+            ContactsList.Items.Add(lbi);
+            lbi = new TextBlock();
+
+            lbi.Text = "Koscher";
+
+            ContactsList.Items.Add(lbi);
+            lbi = new TextBlock();
+
+            lbi.Text = "Crockator";
+
+            ContactsList.Items.Add(lbi);
+
+            lbi = new TextBlock();
+             lbi.Text = " Hi Liber,afhagsfkhgaskgfkahsgfhagsfhgaagsjhfgajhsgfjhagsjfhgajhsfgjahgsfjhagsjfhgasjfhgajhsgfjhahfbznbcmnzbxmcbzmnxbcznbxcmabhsfahsjfhgashfgkasfkjaksjfkajsfkjaskfjgaksfghasgfhgahsfgjhgasfjhagsjf";
+            
+            MessageList.Items.Add(lbi);
+
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
     }
-
-    private void Exit_Click(object sender, RoutedEventArgs e)
-    {
-      NavigationService.GoBack();
-    }
-  }
 }
