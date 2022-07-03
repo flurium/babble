d3cb407be430c77938fc9b561e33ebc4b4c79667
LitvinenkoVitalii <reskator95@gmail.com>
2022-07-03 16:11:56 +0300

    Update CommunicationService.cs
    Add Group
    Leave Group

--- Server/Services/CommunicationService.cs
+++ Server/Services/CommunicationService.cs
@@ -45,17 +45,43 @@ namespace Server.Services
       }
     }
 
-    public void AddGroupHandle(Request req, IPEndPoint ip)
-    { }
+    public async void AddGroupHandle(Request req, IPEndPoint ip)
+    {
+        try
+        {
+            int uid = req.Data.Id;
+            string name = req.Data.Name;
+            await db.AddGroupAsync(uid, name);
+
+            SendData(new Response { Command = Command.AddGroup, Status = Status.OK, Data = "Group added" }, ip);
+        }
+        catch (Exception ex)
+        {
+            SendData(new Response { Command = Command.AddGroup, Status = Status.Bad, Data = ex.Message }, ip);
+        }
+    }
+
+    public async void LeaveGroupHandle(Request req, IPEndPoint ip)
+    {
+        try
+        {
+            int uid = req.Data.Id;
+            string name = req.Data.Name;
+            await db.RemoveUserFromGroupAsync(uid, name);
+
+            SendData(new Response { Command = Command.LeaveGroup, Status = Status.OK, Data = "Group is removed" }, ip);
+        }
+        catch (Exception ex)
+        {
+            SendData(new Response { Command = Command.LeaveGroup, Status = Status.Bad, Data = ex.Message }, ip);
+        }
+    }
 
     public void DisconnectHandle(Request req, IPEndPoint ip)
     {
       int id = req.Data;
       clients.Remove(id);
     }
-
-    public void LeaveGroupHandle(Request req, IPEndPoint ip)
-    { }
 
     public void RemoveContactHandle(Request req, IPEndPoint ip)
     {
