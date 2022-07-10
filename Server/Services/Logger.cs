b9a2527ac30530d4c1d4dbb3da891912f5e6899f
Roman Koshchei <romankoshchei@gmail.com>
2022-07-10 13:37:30 +0300

    remove unnessasary try catch blocks

--- Server/Services/Logger.cs
+++ Server/Services/Logger.cs
@@ -9,17 +9,29 @@ namespace Server.Services
 {
   public interface ILogger
   {
-    void LogException(Request req, Exception ex);
+    void LogException(Exception ex);
+
+    void LogRequest(Request req, Exception ex);
   }
 
   public class Logger : ILogger
   {
-    public void LogException(Request req, Exception ex)
+    public void LogRequest(Request req, Exception ex)
     {
       using (StreamWriter sw = new("exceptions.log", true))
       {
         sw.WriteLine(string.Format("Command: {0}\n", req.Command));
         sw.WriteLine(string.Format("Data: {0}\n", req.Data));
+        sw.WriteLine(string.Format("Exception: {0}\n", ex.Message));
+        sw.WriteLine(string.Format("Help link: {0}\n", ex.HelpLink));
+        sw.WriteLine("-----------------------------------------\n");
+      }
+    }
+
+    public void LogException(Exception ex)
+    {
+      using (StreamWriter sw = new("exceptions.log", true))
+      {
         sw.WriteLine(string.Format("Exception: {0}\n", ex.Message));
         sw.WriteLine(string.Format("Help link: {0}\n", ex.HelpLink));
         sw.WriteLine("-----------------------------------------\n");
