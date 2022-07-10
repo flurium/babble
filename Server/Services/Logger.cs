ea529cbc63b7a5c6b23c58a837b73b13f89088bd
Roman Koshchei <romankoshchei@gmail.com>
2022-07-10 21:25:36 +0300

    logger complete

--- Server/Services/Logger.cs
+++ Server/Services/Logger.cs
@@ -1,40 +1,39 @@
 ﻿using CrossLibrary;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Server.Services
 {
   public interface ILogger
   {
-    void LogException(Exception ex);
+    abstract void LogException(Exception ex);
 
-    void LogRequest(Request req, Exception ex);
+    abstract void LogRequest(Request req, Exception ex);
   }
 
   public class Logger : ILogger
   {
+    public Logger()
+    {
+      if (!Directory.Exists("logs")) Directory.CreateDirectory("logs");
+    }
+
     public void LogRequest(Request req, Exception ex)
     {
-      using (StreamWriter sw = new("exceptions.log", true))
+      using (StreamWriter sw = new("logs\\exceptions.log", true))
       {
-        sw.WriteLine(string.Format("Command: {0}\n", req.Command));
-        sw.WriteLine(string.Format("Data: {0}\n", req.Data));
-        sw.WriteLine(string.Format("Exception: {0}\n", ex.Message));
-        sw.WriteLine(string.Format("Help link: {0}\n", ex.HelpLink));
-        sw.WriteLine("-----------------------------------------\n");
+        sw.WriteLine(string.Format("Command: {0}", req.Command));
+        sw.WriteLine(string.Format("Data: {0}", req.Data));
+        sw.WriteLine(string.Format("Exception: {0}", ex.Message));
+        sw.WriteLine("---------------------------------------------------");
       }
     }
 
     public void LogException(Exception ex)
     {
-      using (StreamWriter sw = new("exceptions.log", true))
+      using (StreamWriter sw = new("logs\\exceptions.log", true))
       {
-        sw.WriteLine(string.Format("Exception: {0}\n", ex.Message));
-        sw.WriteLine(string.Format("Help link: {0}\n", ex.HelpLink));
-        sw.WriteLine("-----------------------------------------\n");
+        sw.WriteLine(string.Format("Exception: {0}", ex.Message));
+        sw.WriteLine(string.Format("Help link: {0}", ex.HelpLink));
+        sw.WriteLine("---------------------------------------------------");
       }
     }
   }
