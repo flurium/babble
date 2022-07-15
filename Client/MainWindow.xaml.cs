63ca22081f6952555fdf810b9d617a18c5fa8853
Roman Koshchei <romankoshchei@gmail.com>
2022-07-15 21:17:22 +0300

    restore main window

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,3 +1,35 @@
 ﻿using Client.Pages;
 using Client.Services;
 using System.Windows;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    ///
+
+    public partial class MainWindow : Window
+    {
+        public static SignInPage signIn;
+        public static SignUpPage signUp;
+        public static UserPage userChat;
+        private readonly CommunicationService cs = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            userChat = new UserPage(cs);
+            signIn = new SignInPage(cs);
+            signUp = new SignUpPage(cs);
+
+            MainFrame.Content = signIn;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            cs.Disconnect();
+        }
+    }
+}
